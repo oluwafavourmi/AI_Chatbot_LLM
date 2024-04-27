fd731c2f98f52b17030b0f0a66dba48f20d502aa
kenny akomolafe <akomolafekenny7@gmail.com>
2024-04-27 22:06:54 +0100

    web.py and requirements files update

--- web.py
+++ web.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Set up the GEMINI API MODEL
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=GOOGLE_API_KEY)
+genai.configure(api_key=AIzaSyAhIOrkXfbcD_LP0uhxoDIYbjy8MMTTarA)
 model = genai.GenerativeModel('gemini-pro')
 
 # function to translate roles between Gemini-pro and streamlit
